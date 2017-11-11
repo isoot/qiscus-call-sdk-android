aaa818f0bd3352bff5561e684216aa3fa0fe4f41
Fitra Aditya <fitra@g.pl>
2017-11-11 07:49:26 +0700

    Add additional library.

--- qiscus-rtc/src/main/java/com/qiscus/rtc/ui/base/BaseActivity.java
+++ qiscus-rtc/src/main/java/com/qiscus/rtc/ui/base/BaseActivity.java
@@ -86,10 +86,10 @@ public abstract class BaseActivity extends AppCompatActivity {
      */
     public void setAlwaysOn() {
         getWindow().addFlags(
-            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 
     /**
