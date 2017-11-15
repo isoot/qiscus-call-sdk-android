7ce6fc76edced082c4f6acfec4dfd1e97b22e457
Fitra Aditya <fitra@g.pl>
2017-11-15 18:48:03 +0700

    Fix missing event.

--- qiscus-rtc/src/main/java/com/qiscus/rtc/engine/hub/WSSignal.java
+++ qiscus-rtc/src/main/java/com/qiscus/rtc/engine/hub/WSSignal.java
@@ -243,7 +243,9 @@ public class WSSignal implements HubSignal, WSChannel.WSChannelEvents {
                     if (data.has("event")) {
                         String evt = data.getString("event");
 
-                        if (evt.equals("call_ack")) {
+                        if (evt.equals("call_sync")) {
+                            channel.ack();
+                        } else if (evt.equals("call_ack")) {
                             if (sender.equals(parameters.target)) {
                                 events.onPnReceived();
                                 channel.setTargetId(parameters.target);
